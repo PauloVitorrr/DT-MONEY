ac0f01a934dde4f71c3f347f2d0bbd8ef372d496
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-12-13 20:34:36 -0300

    feat: componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Olá mundo</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }
